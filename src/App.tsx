389b28a29c43c6c54760b0e96026b81f857d6109
Aleksey Belchenkov <u608110@gmail.com>
2020-05-05 20:57:35 +0300

    Simplify updating values with binding

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,33 @@ import * as React from 'react';
 import Container from 'reactstrap/lib/Container';
 
 import PersonalDetails from './PersonalDetails';
+import { IPersonState } from "./State";
 
 import './App.css';
 
 export default class App extends React.Component {
-  public render() {
-    return (
-        <Container>
-          <PersonalDetails />
-        </Container>
-    );
-  }
+    private defaultPerson : IPersonState = {
+        Address1: "",
+        Address2: null,
+        County: "",
+        DateOfBirth : new Date().toISOString().substring(0,10),
+        FirstName: "",
+        LastName: "",
+        PersonId : "",
+        PhoneNumber: "",
+        Postcode: "",
+        Town: ""
+    };
+
+    constructor(props: {}) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <Container>
+              <PersonalDetails DefaultState={this.defaultPerson} />
+            </Container>
+        );
+    }
 }
--- src/PersonalDetails.tsx
+++ src/PersonalDetails.tsx
@@ -3,7 +3,53 @@ import Button from 'reactstrap/lib/Button';
 import Col from 'reactstrap/lib/Col';
 import Row from 'reactstrap/lib/Row';
 
-export default class PersonalDetails extends React.Component {
+import { IPersonState } from "./State";
+
+interface IProps {
+    DefaultState: IPersonState
+}
+
+export default class PersonalDetails extends React.Component<IProps, IPersonState> {
+    private defaultState: Readonly<IPersonState>;
+
+    constructor(props: IProps) {
+        super(props);
+        this.defaultState = props.DefaultState;
+        this.state = props.DefaultState;
+    }
+
+    private updateBinding = (event: any) => {
+        switch (event.target.id) {
+            case `firstName`:
+                this.setState({ FirstName: event.target.value });
+                break;
+            case `lastName`:
+                this.setState({ LastName: event.target.value });
+                break;
+            case `addr1`:
+                this.setState({ Address1: event.target.value });
+                break;
+            case `addr2`:
+                this.setState({ Address2: event.target.value });
+                break;
+            case `town`:
+                this.setState({ Town: event.target.value });
+                break;
+            case `county`:
+                this.setState({ County: event.target.value });
+                break;
+            case `postcode`:
+                this.setState({ Postcode: event.target.value });
+                break;
+            case `phoneNumber`:
+                this.setState({ PhoneNumber: event.target.value });
+                break;
+            case `dateOfBirth`:
+                this.setState({ DateOfBirth: event.target.value });
+                break;
+        }
+    }
+
     public render() {
         return (
             <Row>
@@ -22,6 +68,8 @@ export default class PersonalDetails extends React.Component {
                                 id="firstName"
                                 className="form-control"
                                 placeholder="First name"
+                                value={this.state.FirstName}
+                                onChange={this.updateBinding}
                             />
                         </Col>
                         <Col>
@@ -30,21 +78,116 @@ export default class PersonalDetails extends React.Component {
                                 id="lastName"
                                 className="form-control"
                                 placeholder="Last name"
+                                value={this.state.LastName}
+                                onChange={this.updateBinding}
                             />
                         </Col>
                     </Row>
                     <Row>
-                        <Col lg="3"><label htmlFor="postcode">Postal/ZipCode</label></Col>
-                        <Col lg="4"><label htmlFor="phoneNumber">Phone number</label></Col>
+                        <Col><label htmlFor="addr1">Address line 1</label></Col>
                     </Row>
                     <Row>
-                        <Col lg="3">
-                            <input type="text" id="postcode" className="form-control" />
-                        </Col>
-                        <Col lg="4">
-                            <input type="text" id="phoneNumber" className="form-control" />
+                        <Col>
+                            <input
+                                type="text"
+                                id="addr1"
+                                className="form-control"
+                                placeholder="Address line 1"
+                                value={this.state.Address1}
+                                onChange={this.updateBinding}
+                            />
                         </Col>
                     </Row>
+                    <Row>
+                        <Col><label htmlFor="addr2">Address line 2</label></Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <input
+                                type="text"
+                                id="addr2"
+                                className="form-control"
+                                placeholder="Address line 2"
+                                value={this.state.Address2!}
+                                onChange={this.updateBinding}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><label htmlFor="town">Town</label></Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <input
+                                type="text"
+                                id="town"
+                                className="form-control"
+                                placeholder="Town"
+                                value={this.state.Town}
+                                onChange={this.updateBinding}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><label htmlFor="county">County</label></Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <input
+                                type="text"
+                                id="county"
+                                className="form-control"
+                                placeholder="County"
+                                value={this.state.County}
+                                onChange={this.updateBinding}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <Col lg="3"><label htmlFor="postcode">Postal/ZipCode</label></Col>
+                                <Col lg="4"><label htmlFor="phoneNumber">Phone number</label></Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <Col lg="3">
+                                    <input
+                                        type="text"
+                                        id="postcode"
+                                        className="form-control"
+                                        value={this.state.Postcode}
+                                        onChange={this.updateBinding}
+                                    />
+                                </Col>
+                                <Col lg="4">
+                                    <input
+                                        type="text"
+                                        id="phoneNumber"
+                                        className="form-control"
+                                        value={this.state.PhoneNumber}
+                                        onChange={this.updateBinding}
+                                    /></Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><label htmlFor="dateOfBirth">Date of birth</label></Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <input
+                                type="date"
+                                id="dateOfBirth"
+                                value={this.state.DateOfBirth!}
+                                onChange={this.updateBinding}
+                            />
+                        </Col>
+                    </Row>
+
                     <Col>
                         <Col>
                             <Row>
